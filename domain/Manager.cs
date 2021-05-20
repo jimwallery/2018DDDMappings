77f37a7b312ec61202b58f572e9332f2566999d9
Jim Wallery <jim.wallery@cloudfusionllc.com>
2021-05-20 13:23:38 -0500

    Back to original

--- domain/Manager.cs
+++ domain/Manager.cs
@@ -13,8 +13,6 @@ namespace Domain {
     public string Name => NameFactory.FullName;
     public int CurrentTeamId { get; private set; }
     public List<ManagerTeamHistory> PastTeams { get; set; }
-    public string JimAdjusted { get; set; }
-
-        //Jim's comment
+   
     }
 }
